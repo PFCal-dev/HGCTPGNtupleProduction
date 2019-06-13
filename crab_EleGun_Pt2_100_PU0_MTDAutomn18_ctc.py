a580ce068e79f0350cb120ed8e3b78b416e8546d
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2019-06-13 21:44:36 +0200

    Update 0PU ctc crab jobs

--- crab_EleGun_Pt2_100_PU0_MTDAutomn18_ctc.py
+++ crab_EleGun_Pt2_100_PU0_MTDAutomn18_ctc.py
@@ -12,14 +12,14 @@ config.General.workArea = 'jobs'
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'produce_ntuple_ctc_reduced_genmatch_v9_cfg.py'
-config.JobType.maxMemoryMB = 2500
+config.JobType.maxMemoryMB = 3000
 config.JobType.maxJobRuntimeMin = 500
 
 config.section_("Data")
 config.Data.inputDataset = '/SingleE_FlatPt-2to100/PhaseIIMTDTDRAutumn18DR-NoPU_103X_upgrade2023_realistic_v2-v1/FEVT'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 2
+config.Data.unitsPerJob = 1
 config.Data.outLFNDirBase = local.outLFNDirBase
 config.Data.publication = False
 config.Data.outputDatasetTag = config.General.requestName
